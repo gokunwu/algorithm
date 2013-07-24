a6673f45c04ac585fc3ad862f03d6254755d3e0e
nciaebupt <nciaebupt@163.com>
2013-07-24 20:05:44 +0800

    new file:   ScarDuck/1.7.10.powerLogn.cpp 	modified:   ScarDuck/1.7.8.permutationsOfSring.cpp 	new file:   ScarDuck/1.7.9.power.cpp 	new file:   ScarDuck/1.8.1.longestPalindrome.cpp 	modified:   ScarDuck/ScarDuck.cbp 	modified:   ScarDuck/ScarDuck.depend 	modified:   ScarDuck/ScarDuck.layout 	modified:   ScarDuck/bin/Debug/ScarDuck.exe 	new file:   ScarDuck/obj/Debug/1.7.10.powerLogn.o 	modified:   ScarDuck/obj/Debug/1.7.7.minNumInRotateArray.o 	new file:   ScarDuck/obj/Debug/1.7.8.permutationsOfSring.o 	new file:   ScarDuck/obj/Debug/1.7.9.power.o 	new file:   ScarDuck/obj/Debug/1.8.1.longestPalindrome.o

--- ScarDuck/1.7.8.permutationsOfSring.cpp
+++ ScarDuck/1.7.8.permutationsOfSring.cpp
@@ -1,4 +1,4 @@
-
+#if 0
 /*
 copyright@nciaebupt 转载请注明出处
 题目：输入一个字符串，打印出该字符串中字符的所有排列。例如输入字符串abc，则输出由字符a、b、c所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
@@ -6,36 +6,102 @@ copyright@nciaebupt 转载请注明出处
 对于字符串的排列问题。如果能生成
 n-1个元素的全排列，就能生成n个元素的全排列。对于只有1个元素的集合，可以直接生成全排列。全排列的递归终止条件很明确，只有1个元素时。
 */
+#include <cstring>
 #include <cstdlib>
 #include <iostream>
 
-void permutationsOfString(char * str, int begin, int end)
-{
-  if(str == NULL || len < 1){
+void exchange(char * str, int i, int j){
+  if(str == NULL){
     return;
   }
-  if(begin == end -1){
+  char tmp = str[i];
+  str[i] = str[j];
+  str[j] = tmp;
+}
 
+void permutationsOfString(char * str, int begin, int end)
+{
+  if(str == NULL || begin > end){
+    return;
+  }
+  if(begin == end){
+    std::cout<<str<<std::endl;
+  }
+  else{
+    for(int i = begin; i <= end; ++i){
+      exchange(str, begin, i);
+      permutationsOfString(str, begin + 1, end);
+      exchange(str, begin, i);
+    }
   }
 }
 
 int main(int argc, char ** argv)
 {
   char str[]="abc";
-  int len = sizeof(str)/sizeof(str[0]);
+  int len = strlen(str);
 
   permutationsOfString(str, 0, len -1);
 
   system("pause");
   return 0;
 }
+#endif
 
 
-
-
+#if 0
 /*
 copyright@nciaebupt 转载请注明出处
 问题：输入一个字符串，输出该字符串中字符的所有组合。举个例子，如果输入abc，它的组合有a、b、c、ab、ac、bc、abc。
 分析：用递归求解。可以考虑求长度为n的字符串中m个字符的组合，设为C(n,m)。原问题的解即为C(n, 1), C(n, 2),...C(n, n)的总和。
 对于求C(n,m)，从第一个字符开始扫描，每个字符有两种情况，要么被选中，要么不被选中，如果被选中，递归求解C(n-1, m-1)。如果未被选中，递归求解C(n-1, m)。不管哪种方式，n的值都会减少，递归的终止条件n=0或m=0。
 */
+
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+void combinationsOfStr(char * str, int size, std::vector<char> & chvec){
+  if(str == NULL || (size != 0 && *str == '\0')){
+    return;
+  }
+  if(size == 0){
+    for(int i = 0; i < chvec.size(); ++i){
+      std::cout<<chvec[i];
+    }
+    std::cout<<std::endl;
+    return;
+  }
+  chvec.push_back(*str);
+  combinationsOfStr(str+1, size - 1, chvec);
+  chvec.pop_back();
+  combinationsOfStr(str+1, size, chvec);
+}
+
+void combinationsOfStr(char * str, int len){
+  if(str == NULL || len < 1){
+    return ;
+  }
+  for(int i = 1; i <= len; ++i){
+    std::vector<char> chvec;
+    combinationsOfStr(str, i, chvec);
+  }
+}
+
+int main(int argc, char ** argv){
+  char str[] = "abc";
+  int len = strlen(str);
+
+  combinationsOfStr(str, len);
+
+  system("pause");
+  return 0;
+}
+#endif
+
+
+
+
+
+
---
+++ ScarDuck/1.7.9.power.cpp
@@ -0,0 +1,69 @@
+#if 0
+/*
+copyright@nciaebupt 转载请注明出处
+题目：实现函数double Power(double base,int exponent)，求base的exponent次方。
+不得使用库函数，同时不需要考虑大树问题。
+分析：
+这道题目有以下几点需要注意：
+1.  0的0次方是无意义的，非法输入
+2.  0的负数次方相当于0作为除数，也是无意义的，非法输入
+3.  base如果非0，如果指数exponent小于0，可以先求base的|exponent|次方，然后再
+求倒数
+4.  任何数的0次方都为0
+5.  判断double类型的base是否等于0不能使用==号。因为计算机表述小树(包括float
+和double型小数)都有误差，不能直接使用等号(==)判断两个小数是否相等。如果两
+个数的差的绝对值很小，那么可以认为两个double类型的数相等。
+*/
+#include <cstdlib>
+#include <iostream>
+
+bool invalidInput = false;
+
+bool doubleEqual(double x, double y)
+{
+  if(((x > y) && (x-y < 0.000001)) || ((x < y) && (x-y > -0.000001))){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+double Power(double base, int exponent){
+  if(doubleEqual(base, 0.0) && exponent <= 0){//0的0次方与0的负次方 无效输入
+    invalidInput = true;
+    return -1.0;
+  }
+  if(exponent == 0){//任何数的零次方都为0
+    return 0.0;
+  }
+  //判断指数是否为负数
+  int negative = false;
+  if(exponent < 0){
+    negative = true;
+    exponent = -exponent;
+  }
+  double res = 1.0;
+  for(int i = 0; i < exponent; ++i){
+    res = res * base;
+  }
+  if(negative == true){
+    return 1/res;
+  }
+  else{
+    return res;
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  double base = 2.0;
+  int exponent = -2;
+
+  double res = Power(base, exponent);
+  std::cout<<res<<std::endl;
+
+  system("pause");
+  return 0;
+}
+#endif
